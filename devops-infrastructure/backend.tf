b463d685f60f88c97317eb034f992aab53db5632
MUTHUSURESH N <muthusuresh.240198@aids.ritchennai.edu.in>
2026-09-19 20:47:58 +0530

    terraform file updated

--- devops-infrastructure/backend.tf
+++ devops-infrastructure/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "terraform-demo-s3-5"
     key          = "devops-infrastructure/terraform.tfstate"
-    region       = "ap-south-1"
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
